b3cdfc6d41eb9390a9eaa2076fb506b8e2ef8743
shvardhan1994 <54303315+shvardhan1994@users.noreply.github.com>
2019-09-13 12:06:30 +0200

    Initial commit of the newly created LAB 3 Package.

---
+++ Lab3Package/R/R_LAB03_Assignment.R
@@ -0,0 +1,19 @@
+euclidean <- function(a,b){
+i <- max(a,b)
+j <- min(a,b)
+while(j > 0){
+  i <- j * floor((max(i,j) / min(i,j))) + max(i,j) %% min(i,j)
+  r <- max(i,j) %% min(i,j)
+  if(r == 0){
+    print(min(i,j))
+    break
+  }
+  i <- j
+  j <- r
+  
+}
+}
+
+
+
+
