b991f225a49845f36da7c3e53d2c3c21a2772185
shvardhan1994 <54303315+shvardhan1994@users.noreply.github.com>
2019-09-15 19:09:08 +0200

    All the Test cases are passing.

--- Lab3Package/R/euclidean.R
+++ Lab3Package/R/euclidean.R
@@ -9,21 +9,20 @@
 
 
 euclidean <- function(a,b){
-i <- max(a,b)
-j <- min(a,b)
-while(j > 0){
-  i <- j * floor((max(i,j) / min(i,j))) + max(i,j) %% min(i,j)
-  r <- max(i,j) %% min(i,j)
-  if(r == 0){
-    print(min(i,j))
-    break
+  i <- max(a,b)
+  j <- min(a,b)
+  while(j != 0){
+    
+    r <- max(i,j) %% min(i,j)
+    if(r == 0 ){
+      return(abs(min(i,j)))
+      break
+    }
+    i <- j
+    j <- r
+    
   }
-  i <- j
-  j <- r
-  
 }
-}
-
 
 
 
