976238b8cf4c606c9a294504a13c7d5f9eddd329
shvardhan1994 <54303315+shvardhan1994@users.noreply.github.com>
2019-09-18 23:20:59 +0200

    Adding wiki_graph dataset.

---
+++ Lab3Package/R/data.R
@@ -0,0 +1,11 @@
+#' Wikipedia Graph
+#' @description The data set is taken from wikipedia dijsktra algorithm page and included in the package.
+#' @format \code{wiki_graph} is data.frame with 18 rows and 3 variables:
+#' \describe{
+#' \item{v1}{starting node, no unit}
+#' \item{v2}{ending node, no unit}
+#' \item{w}{weight of the vertex between \code{v1} and \code{v2} , no unit}
+#' }
+#' 
+#' @source \href{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}{Wikipedia}
+"wiki_graph"
